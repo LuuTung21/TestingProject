f84ca111e7bd66b4ce7f9095e2ddf15c10467094
LuuTung21 <luutung210443@gmail.com>
2023-08-15 11:09:05 +0900

    Initialize MongoDB Connection

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import connectDB from "./config/db.js";
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+})
