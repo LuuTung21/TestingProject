200b996219432ac0b5beb26940196e31fbd13bdc
LuuTung21 <luutung210443@gmail.com>
2023-08-28 23:24:46 +0900

    Updating LinkContainter for Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
 import './App.css'
 import Header from './components/Header'
 import { Outlet } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
 
 function App() {
 
   return (
     <>
       <Header />
-      <Outlet />
+      <Container className='my-2'>
+        <Outlet />
+      </Container>
     </>
   )
 }
 
-export default App
+export default App;
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,25 +2,34 @@ import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { AiOutlineLogin, AiOutlineUserAdd } from 'react-icons/ai';
 import { RiShoppingCart2Line } from 'react-icons/ri';
+import { LinkContainer } from "react-router-bootstrap"
 
 const Header = () => {
     return (
         <Navbar bg="light" expand="lg" className="px-4">
-            <Navbar.Brand href="/">
-                Shoppable
-            </Navbar.Brand>
+            <LinkContainer to="/">
+                <Navbar.Brand >
+                    Shoppable
+                </Navbar.Brand>
+            </LinkContainer>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
                 <Nav>
-                    <Nav.Link href="/login">
-                        <AiOutlineLogin size={20} /> Login
-                    </Nav.Link>
-                    <Nav.Link href="/signup">
-                        <AiOutlineUserAdd size={20} /> Sign Up
-                    </Nav.Link>
-                    <Nav.Link href="/cart">
-                        <RiShoppingCart2Line size={20} /> Cart
-                    </Nav.Link>
+                    <LinkContainer to="/login">
+                        <Nav.Link>
+                            <AiOutlineLogin size={20} /> Login
+                        </Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to="/signup">
+                        <Nav.Link>
+                            <AiOutlineUserAdd size={20} /> Sign Up
+                        </Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to="/cart">
+                        <Nav.Link>
+                            <RiShoppingCart2Line size={20} /> Cart
+                        </Nav.Link>
+                    </LinkContainer>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
