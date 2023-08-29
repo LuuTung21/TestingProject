7b5d786983e50fe9c78bdfd4dcfa59c9f0969a34
LuuTung21 <luutung210443@gmail.com>
2023-08-29 22:01:57 +0900

    Update Hero Componet for displaying Home Screen

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Hero = () => {
     return (
-        <Container fluid className="home-screen">
-            <Row className="align-items-center justify-content-center vh-100">
-                <Col xs={12} md={8} lg={6} xl={4} className="text-center">
-                    <h1 className="mb-4">Welcome to Our E-Commerce Store</h1>
-                    <p className="mb-4">Discover amazing products at great prices.</p>
-                    <div className="d-flex justify-content-center">
-                        <Button variant="primary" className="mr-3">
-                            <a href="/signup" className="text-white text-decoration-none">Sign Up</a>
-                        </Button>
-                        <Button variant="outline-primary">
-                            <a href="/login" className="text-primary text-decoration-none">Log In</a>
-                        </Button>
-                    </div>
-                </Col>
-            </Row>
-        </Container>
+        <div className="hero-bg">
+            <Container fluid className="hero-container">
+                <Row className="align-items-center justify-content-center vh-100">
+                    <Col xs={12} md={8} lg={6} xl={4} className="text-center">
+                        <h1 className="hero-title">Welcome to Our E-Commerce Store</h1>
+                        <p className="hero-description">Discover amazing products at great prices.</p>
+                        <div className="hero-buttons">
+                            <LinkContainer to="/signup">
+                                <Button variant="primary" className="mr-3">Sign Up</Button>
+                            </LinkContainer>
+                            <LinkContainer to="/login">
+                                <Button variant="outline-primary">Log In</Button>
+                            </LinkContainer>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     );
 };
 
